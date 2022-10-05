0e399b716e1892ea5efe3f54ae2bfb60b8a7df65
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-05 16:53:21 -0500

    6

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 addEventListener("DOMContentLoaded",()=>{
-//     5. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
-// de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
-// nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
-// hacer el bizcocho es
-document.write(`5. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para hacer el bizcocho es<br>`)
-let harina=100,cacao=10,Tlharina=0
-let grcacao=Number(prompt("Ingrese los gramos de cacao"))
-Tlharina=(harina*grcacao)/cacao
-document.write(`La cantidad de harina que se necesita para hacer el bizcocho con ${grcacao} gr de cacao es de: ${Tlharina} `)
+    // 6. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
+    // transporta ?
+    document.write(`26. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg transporta ?<br>`)
+    let cajas=25,peso=748,pesoT=0
+    pesoT=(cajas*peso)
+    document.write(`El peso total que transporta el camion es de: ${pesoT}`)
 
 
 })
