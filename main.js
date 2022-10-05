d6c03a042bf8a3ad5a97beb4b9a36df00c7fe144
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-05 18:27:00 -0500

    10

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
 addEventListener("DOMContentLoaded",()=>{
-    //9. Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
-    //pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
-    //que representen el algoritmo para solucionar este problema.
-    //Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula
-    //sostenida y la edad del entrevistado.
-    let nom=" ",edad=0,person=0
-    person=Number(prompt("Ingrese la cantidad de personas que desea determinar su edad: "))
-    let i=0
-    while(i<=person){
-        i++
-        nom=prompt("Ingrese el nombre de la persona: ")
-        edad=Number(prompt("Ingrese el año de nacimiento de la persona : "))
-
-        let edper=(2022-edad)
-        document.write(`${nom} tiene: ${edper}<br>`)
-    }
+// 10. Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase
+// invertido.
+let frase=prompt("Ingrese una frase que lo identifique como programador de Software")
+document.write(`${frase}<br>`)
+let frasein = frase.split("").reverse().join("")
+document.write(`La frase invertida queda : ${frasein}`)
 })
