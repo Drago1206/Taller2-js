961e6aa002f14731e3284b776133fb309921cafe
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-05 16:04:11 -0500

    1

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 addEventListener("DOMContentLoaded",()=>{
-    let n1=0 , n2=0 , n3=0
-    n1=Number(prompt("Ingrese el primer numero:  ",3));
-    n2=Number(prompt("Ingrese el segundo numero:  ",4));
-    n3=Number(prompt("Ingrese el tercer numero: ",5));
-    console.log(`La suma es de ${n1} + ${n2} + ${n3} = ${n1+n2+n3}`);
-    console.log(`La resta es de ${n1} - ${n2} - ${n3} =${n1-n2-n3}`);
-    console.log(`La multiplicacion es ${n1} * ${n2} * ${n3} = ${n1*n2*n3}`);
-    console.log(`La division es de ${n1} / ${n2} / ${n3} = ${n1/n2/n3}`);
+      // 21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello
+    // d = rc((x2-x1)^2 + (y2-y1)^2)
+document.write(`21. Procesos para calcular la distancia entre dos puntos...<br>`)
+let rc=0,x1=0,x2=0,y1=0,y2=0,D=0
+rc=Number(prompt("Ingrese el valor de rc"))
+x1=Number(prompt("Ingrese el valor de x1"))
+x2=Number(prompt("Ingrese el valor de x2"))
+y1=Number(prompt("Ingrese el valor de y1"))
+y2=Number(prompt("Ingrese el valor de y2"))
+D=rc*((x2-x1)^2 + (y2-y1)^2)
+document.write(`La distancia entre los dos puntos es de : ${D}`)
 })
