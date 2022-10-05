cdf5c8a742e16b36a2b85f5c8d7b79efa70a5e55
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-05 16:59:42 -0500

    7

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 addEventListener("DOMContentLoaded",()=>{
-    // 6. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg
-    // transporta ?
-    document.write(`26. Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg transporta ?<br>`)
-    let cajas=25,peso=748,pesoT=0
-    pesoT=(cajas*peso)
-    document.write(`El peso total que transporta el camion es de: ${pesoT}`)
-
+    // 7. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
+    // 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
+    // Lorena?
+    document.write(`27. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a Lorena?<br>`)
+    let Martin=2/3,Jairo=1/4,Lorena=1/1,suma=0
+    suma=(Martin+Jairo)
+    let coresponde=(Lorena-suma)
+document.write(`Jairo y Martin tiene un total de: ${suma}<br>`)
+document.write(`A lorena le corresponde  un total de: ${coresponde}<br>`)
 
 })
