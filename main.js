e0dcdb087b5c71444dc4c49d560fcbb382aa077a
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-05 16:49:59 -0500

    5

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 addEventListener("DOMContentLoaded",()=>{
-// 4. Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
-    // $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
-    // sobran $91000. ¿cuánto dinero tenía?
-    document.write(`24. Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le sobran $91000. ¿cuánto dinero tenía?<br>`)
-    let hex=0,bomba=0,caja=0,sobrante=0
-    hex = 11500
-    bomba = 1168000
-    caja = 87000
-    sobrante = 91000
-    let C_hex=0,C_caja=0,total=0
-    C_hex = hex*5
-    C_caja = caja*3
-    total = sobrante+C_caja+C_hex+bomba
-    document.write(`el total de dinero que tenia es de:  ${total}`);
+//     5. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
+// de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
+// nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
+// hacer el bizcocho es
+document.write(`5. En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para hacer el bizcocho es<br>`)
+let harina=100,cacao=10,Tlharina=0
+let grcacao=Number(prompt("Ingrese los gramos de cacao"))
+Tlharina=(harina*grcacao)/cacao
+document.write(`La cantidad de harina que se necesita para hacer el bizcocho con ${grcacao} gr de cacao es de: ${Tlharina} `)
+
+
 })
